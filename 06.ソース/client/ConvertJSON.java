53bf4b64d6320fb030fa103d7573e2a9b54093ac
ptechatsugai <hatsugai@p-tec.jp>
2017-08-28 18:08:50 +0900

    Brainの情報を利用できない問題を解決しました。また、BrainBeanをjar内のみにしました。

--- 06.ソース/client/ConvertJSON.java
+++ 06.ソース/client/ConvertJSON.java
@@ -18,10 +18,9 @@ public class ConvertJSON {
 	 * @param IPAdress 動作させているマシンのIPアドレスです。サーバー側でクライアントにデータを送るときに使用します
 	 * @return ロジック情報を格納したJSONObjectです
 	 */
-	public JSONObject convertToJSONF(String IPAdress){
+	public JSONObject convertToJSONF(List<BrainBean> logList,String IPAdress){
 		JSONObject obj = new JSONObject();
-		List<BrainBean> brab = new ArrayList<BrainBean>();
-		for(BrainBean brb:brab){
+		for(BrainBean brb:logList){
 			obj.put("logicName", brb.getLogicName());
 			obj.put("logicVersion", brb.getLogicVersion());
 			obj.put("writer", brb.getWriter());
@@ -33,7 +32,7 @@ public class ConvertJSON {
 	 * JSONObjectからデータを取り出してBattleInfoBeanに格納します
 	 * @param rcvmsg サーバーから送られてきたJSONObjectです
 	 */
-	public void convertFromJSON(JSONObject rcvmsg){
+	public List<BattleInfoBean> convertFromJSON(JSONObject rcvmsg){
 		List<BattleInfoBean> bl = new ArrayList<BattleInfoBean>();
 		BattleInfoBean bib = new BattleInfoBean();
 		//エラー条件や終了条件がメッセージ中に含まれていた場合にeventにその旨を格納します。
@@ -48,14 +47,14 @@ public class ConvertJSON {
 			bib.setLocation((String[][])rcvmsg.get("location"));
 		}
 		bl.add(bib);
+		return bl;
 	}
 	/**
 	 * 指し手情報をJSONObjectに変換します
 	 * @return 指し手情報を格納したJSONObjectです
 	 */
-	public JSONObject convertToJSONS(){
+	public JSONObject convertToJSONS(List<BattleInfoBean> bibn){
 		JSONObject obj = new JSONObject();
-		List<BattleInfoBean> bibn = new ArrayList<BattleInfoBean>();
 		//指し手情報をBattleInfoBeanから取り出して格納します。
 		for(BattleInfoBean bib:bibn){
 			obj.put("xAxis", bib.getxAxis());
